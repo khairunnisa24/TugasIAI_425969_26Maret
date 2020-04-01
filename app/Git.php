be2a8bbc75d4f43e8ea88f0289838d9a61a98112
Khairunnisa <khairunnisa00@mail.ugm.ac.id>
2020-04-01 23:11:53 +0700

    push ulang

--- app/Git.php
+++ app/Git.php
@@ -6,7 +6,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
-class User extends Model
+class git extends Model
  {   
     protected $fillable = [
         'id', 'user_id', 'git', 'created_at', 'updated_at',
